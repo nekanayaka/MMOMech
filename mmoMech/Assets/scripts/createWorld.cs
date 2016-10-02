3176baa7c6bbc038f9502592ebe4c8c132de4c5f
HC\ejones109029 <elkeno_q.jones@hotmail.com>
2016-10-02 15:51:51 -0300

    Creation of greatness

---
+++ mmoMech/Assets/scripts/createWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    //create world foundation: 
+        //create spawn points    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
